aed04b8bac47cd7503c4f00232b31df55214c984
urfanadjie <hawaiiditamang@gmail.com>
2023-05-21 07:57:39 +0700

    Update : Penyempurnaan SplashScreen

--- lib/get_started.dart
+++ lib/get_started.dart
@@ -15,7 +15,6 @@ class GetStarted extends StatelessWidget {
                 height: 16,
                 child: const Text("Get Fly like a Bird", style: TextStyle(color: Colors.blue, fontSize: 24, fontWeight: FontWeight.bold))
             )
-
           ],
         ),
       ),
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+import 'home.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+  class _SplashScreenState extends State<SplashScreen> {
+    @override
+    void initState() {
+      super.initState();
+      _navigateToHome();
+    }
+
+    _navigateToHome()async{
+      await Future.delayed(const Duration (milliseconds: 3000), () {} );
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyHomePage(title: "AirplaneApp")));
+    }
 
   @override
   Widget build(BuildContext context) {
